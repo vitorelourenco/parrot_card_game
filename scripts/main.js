36c325f8ca8beb59fb071e45dffecae5691ac566
Vitor Lourenco <vitor.registros@lourencos.net>
2021-04-07 08:14:50 -0300

    Add game functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,79 +1,164 @@
+//[{id:'n', src:'url'},...]
 const cardsObj = [
   {
-    id: '1',
-    src: 'assets/images/bobrossparrot.gif'
+    id: 'img1',
+    src: 'assets/images/bobrossparrot.gif',
+    alt: 'bobross parrot'
   },
   {
-    id: '2',
-    src: 'assets/images/explodyparrot.gif'
+    id: 'img2',
+    src: 'assets/images/explodyparrot.gif',
+    alt: 'explody parrot'
   },
   {
-    id: '3',
-    src: 'assets/images/fiestaparrot.gif'
+    id: 'img3',
+    src: 'assets/images/fiestaparrot.gif',
+    alt: 'fiesta parrot'
   },
   {
-    id: '4',
-    src: 'assets/images/metalparrot.gif'
+    id: 'img4',
+    src: 'assets/images/metalparrot.gif',
+    alt: 'metal parrot'
   },
   {
-    id: '5',
-    src: 'assets/images/revertitparrot.gif'
+    id: 'img5',
+    src: 'assets/images/revertitparrot.gif',
+    alt: 'revertit parrot'
   },
   {
-    id: '6',
-    src: 'assets/images/tripletsparrot.gif'
+    id: 'img6',
+    src: 'assets/images/tripletsparrot.gif',
+    alt: 'triplets parrot'
   },
   {
-    id: '7',
-    src: 'assets/images/unicornparrot.gif'
+    id: 'img7',
+    src: 'assets/images/unicornparrot.gif',
+    alt: 'unicorn parrot'
   }
 ];
 
+function wait1000() {
+  console.log('clicked');
+}
 
 function newGame(){
+
   //loops until a valid input is given
-  //returns the integer number of pairs
-  function readPairs(){
-    //checks to see if the input is valid (2<=n<=7)
+  //returns the integer number of cards
+  function readCards(){
+
+    //checks to see if the input is valid (4<=n<=17)
     //returns true/false
     function checkInput(str){
       if (typeof(str) !== 'string') return false;
       if (str === '') return false;
       if (/\D/.test(str)) return false;
-      if (parseInt(str, 10)<2 || parseInt(str, 10)>7) return false;
+      const val = parseInt(str, 10);
+      if (val<4 || val>14) return false;
+      if (val%2 !== 0) return false;
       return true;
     }
 
-    let strPairs;
+    let strCards;
     do{
-      strPairs = prompt('Com quantos pares quer jogar? (2 a 7)');
-    } while (!checkInput(strPairs))
-    return parseInt(strPairs, 10);
+      strCards = prompt('Com quantas cartas voce quer jogar? (4,6,8,10,12,14)');
+    } while (!checkInput(strCards))
+    return parseInt(strCards, 10);
   }
 
-  //flushes out any HTML in the game container
+  //flush out anything from an older game
+  //and set fresh vars
   gameContainer.innerHTML = '';
+  selectedList = [];
+  flipCount = 0;
+  lastCheckTime = Date.now();
 
+  //get the number of cards for the new instance of the game
+  const nCards = readCards();
 
-  
+  //change this later to get n random cards
+  const nCardsArr = cardsObj.slice(0,nCards);
+
+  //duplicate each card obj and store it in gameArr
+  const gameArr = nCardsArr.concat(nCardsArr);
+
+  //shuffle gameArr
+  function shuffling(){};
+
   //builds the HTML for a new instance of the game
-  const nPairs = readPairs();
-  for (let i=0; i<nPairs*2; i++){
+  for (let i=0; i<gameArr.length; i++){
     gameContainer.innerHTML += 
-    `<div class="card">
+    `<div class="card ${gameArr[i].id} index${i}" onclick="handleCardClick('.${gameArr[i].id}', ${i})">
       <div class="card-content front">
-        <img src="assets/images/front.png" alt="">
+        <img src="assets/images/front.png" alt="parrot">
       </div>
       <div class="card-content back">
-        <img src="" alt="">
+        <img src="${gameArr[i].src}" alt="${gameArr[i].alt}">
       </div>
     </div>`;
   }
 
+  allCards = gameContainer.querySelectorAll('.card');
+}
+
+function handleCardClick(callerClass, callerIndex){
+  function flipUp(card){
+    const cardBack = card.querySelector('.back');
+    const cardFront = card.querySelector('.front');
+    cardBack.classList.add('back-flip');
+    cardFront.classList.add('front-flip');
+  }
+  function flipDown(card){
+    const cardBack = card.querySelector('.back');
+    const cardFront = card.querySelector('.front');
+    cardBack.classList.remove('back-flip');
+    cardFront.classList.remove('front-flip');
+  }
+
+  //force the play to wait if they get it wrong
+  if (Date.now()-lastCheckTime<1100) return;
+
+  const selected = allCards[callerIndex];
+  //ignore it if the player clicks a card that has already been discovered
+  if (selected.classList.contains('permanent')) return;
+
+  flipCount++;
+
+  flipUp(selected);
+
+  selectedList.push({id: callerClass, index: callerIndex});
+
+  if (selectedList.length == 2){
+    lastCheckTime = Date.now();
+
+    const previousSelected = allCards[selectedList[0].index];
+
+    if (selectedList[0].id == selectedList[1].id){
+      selected.classList.add('permanent');
+      previousSelected.classList.add('permanent');
+      lastCheckTime = Date(0,0,0,0,0,0);
+    } else {
+      setTimeout(()=>{
+        flipDown(selected);
+        flipDown(previousSelected);
+      }, 1000);
+    }
+
+    selectedList = [];
+  }
+
+  if (document.querySelectorAll('.permanent').length == document.querySelectorAll('.card').length){
+    alert(`Voce ganhou em ${flipCount} jogadas e em X segundos`)
+    const newGameAnswer = prompt('Quer Jogar de novo? digite sim');
+    if (newGameAnswer === 'sim'){
+      newGame();
+    }
+  }
 }
 
 const gameContainer = document.querySelector('.cards-container');
+let lastCheckTime;
+let allCards;
+let selectedList;
+let flipCount;
 newGame();
-
-
-
